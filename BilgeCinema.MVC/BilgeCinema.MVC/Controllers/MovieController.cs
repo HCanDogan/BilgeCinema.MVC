2567c6107c75b0ae0a2f9e9d66961c9ddd5e35c8
Hamdi Can Doğan <118393382+HCanDogan@users.noreply.github.com>
2023-12-10 20:46:27 +0300

    BilgeCinema.MVC

---
+++ BilgeCinema.MVC/BilgeCinema.MVC/Controllers/MovieController.cs
@@ -0,0 +1,103 @@
+﻿using BilgeCinema.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilgeCinema.MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly AppSettings _appSettings;
+        public MovieController(AppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public IActionResult New()
+        {
+            return View("Form");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var getUrl = $"{_appSettings.ApiBaseUrl}/movies/{id}";
+
+            using var client = new HttpClient();
+
+            var response = await client.GetFromJsonAsync<MovieFormViewModel>(getUrl);
+
+            return View("Form", response);
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(MovieFormViewModel formData)
+        {
+            if(formData.Id == 0) 
+            {
+                // var insertUrl = "https://localhost:7085/api/movies"
+                var insertUrl = $"{_appSettings.ApiBaseUrl}/movies";
+
+                using var client = new HttpClient();
+
+                var response = await client.PostAsJsonAsync(insertUrl, formData);
+          
+
+                if(response.IsSuccessStatusCode) 
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Film kayıt edilirken bir hata oluştu.";
+                    return View("Form", formData);
+                }
+
+               
+            }
+            else 
+            {
+                var updateUrl = $"{_appSettings.ApiBaseUrl}/movies/{formData.Id}";
+
+                using var client = new HttpClient();
+
+                var response = await client.PutAsJsonAsync(updateUrl, formData);
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Film güncellenirken bir hata oluştu.";
+                    return View("Form", formData);
+                }
+
+            }
+            
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleteUrl = $"{_appSettings.ApiBaseUrl}/movies/{id}";
+
+            using var client = new HttpClient();
+
+            var response = await client.DeleteAsync(deleteUrl);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> MakeDiscount(int id)
+        {
+            var patchUrl = $"{_appSettings.ApiBaseUrl}/movies/{id}";
+
+            using var client = new HttpClient();
+
+            var response = await client.PatchAsync(patchUrl, null);
+
+            return RedirectToAction("Index", "Home");
+
+
+        }
+    }
+}
---
+++ BilgeCinema.MVC/BilgeCinema.MVC/Program.cs
@@ -0,0 +1,19 @@
+using BilgeCinema.MVC.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+
+
+var settings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>(); 
+
+builder.Services.AddSingleton(settings);
+
+var app = builder.Build();
+
+
+app.MapDefaultControllerRoute();
+
+app.Run();
